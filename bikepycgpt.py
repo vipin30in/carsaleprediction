580ce5da1096ab4c0c38ea833624be2a52c6021d
vipin30in <vipin30in@gmail.com>
2025-01-15 16:00:56 +0530

    Update bikepycgpt.py

--- bikepycgpt.py
+++ bikepycgpt.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import scikit-learn
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error
 
